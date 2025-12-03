b186c3b57d7f42139ee281ed1b992049b29f25fc
Srividya Subramanian <srividya.ssa@gmail.com>
2025-12-03 13:06:39 +0100

    Day 3

---
+++ 2025/3/joltage.py
@@ -0,0 +1,51 @@
+BATS = 100
+
+
+def joltage1(banks):
+    total = 0
+    for bank in banks:
+        max_bat, max_i = bank[0], 0
+        for i in range(1, BATS - 1):
+            if bank[i] > max_bat:
+                max_bat = bank[i]
+                max_i = i
+        digit1 = max_bat
+
+        max_i += 1
+        max_bat = bank[max_i]
+        for i in range(max_i, BATS):
+            if bank[i] > max_bat:
+                max_bat = bank[i]
+        digit2 = max_bat
+
+        total += int(digit1 + digit2)
+
+    return total
+
+
+def joltage2(banks):
+    SELECT = 12
+
+    total = 0
+    for bank in banks:
+        mono = []
+        skip = BATS - SELECT
+
+        for b in bank:
+            while mono and b > mono[-1] and skip > 0:
+                mono.pop()
+                skip -= 1
+            mono.append(b)
+
+        total += int("".join(mono[:SELECT]))
+
+    return total
+
+
+if __name__ == "__main__":
+    banks = None
+    with open("in.txt", "r") as file:
+        banks = file.read().strip().split("\n")
+
+    print(joltage1(banks))
+    print(joltage2(banks))
